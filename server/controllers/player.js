17f813b6c7113de3bfd476091972682ec9efe122
rehanp1 <rehanpathan8012@gmail.com>
2025-06-20 01:01:53 +0530

    Player controller CRUD operations

--- server/controllers/player.js
+++ server/controllers/player.js
@@ -32,3 +32,69 @@ export const createPlayer = async (req, res) => {
     res.status(500).json({ success: false, err });
   }
 };
+
+export const getAllPlayer = async (req, res) => {
+  try {
+    const result = await Player.find({});
+
+    res.status(200).json({ success: true, result });
+  } catch (err) {
+    res.status(500).json({ success: false, err });
+  }
+};
+
+export const deletePlayer = async (req, res) => {
+  try {
+    const { playerId } = req.params;
+
+    const playerExists = await Player.findOne({ _id: playerId });
+
+    if (!playerExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Player does not exists" });
+    }
+
+    await Player.deleteOne({ _id: playerId });
+
+    res.status(200).json({ success: true, message: "Player deleted !" });
+  } catch (err) {
+    res.status(500).json({ success: false, err });
+  }
+};
+
+export const updatePlayer = async (req, res) => {
+  try {
+    const { name, role } = req.body;
+    const { playerId } = req.params;
+
+    if (!name || !role) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Name or Role is not provided" });
+    }
+
+    const playerExist = await Player.findOne({ _id: playerId });
+
+    if (!playerExist) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Player does not exist" });
+    }
+
+    const fileUploaded = await uploadOnCloudinary(req.file.path);
+
+    playerExist.name = name;
+    playerExist.role = role;
+    playerExist.avatar = fileUploaded.secure_url;
+
+    await playerExist.save();
+
+    res.status(200).json({
+      success: true,
+      result: { name, role, avatar: fileUploaded.secure_url },
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, err });
+  }
+};
--- server/routes/player.js
+++ server/routes/player.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { createPlayer } from "../controllers/player.js";
+import {
+  createPlayer,
+  deletePlayer,
+  getAllPlayer,
+  updatePlayer,
+} from "../controllers/player.js";
 import upload from "../middleware/multer.js";
 
 const playerRouter = Router();
 
 playerRouter.post("/create", upload.single("avatar"), createPlayer);
+playerRouter.get("/getall", getAllPlayer);
+playerRouter.post("/delete/:playerId", deletePlayer);
+playerRouter.post("/update/:playerId", upload.single("avatar"), updatePlayer);
 
 export default playerRouter;
